ea5c5dae9bec2279ac672e81a1a419d0d492f18c
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-05-24 14:02:45 +0530

    Assignment one of data types

--- src/Assignment_DataType.java
+++ src/Assignment_DataType.java
@@ -3,6 +3,26 @@ public class Assignment_DataType {
 
 	public static void main(String[] args) {
 		
+		double citytemp = 25.5;
+		 boolean customer_order = true;
+		 String customer_phone= "123-456-7890";
+		 double customer_amountinbank=1000.50;
+		 String cutomer_email="john.doe@example.com";
+		 boolean marital_status=false;
+		 String occupation ="Software Engineer";
+		 String favourite_colour= "Blue";
+		 int current_year=2025;
+		 int followers_socialmedia =1000000;
+		  double movie_rating=7.5;
+		char blood_type='A';
+		String booktitle= "To Kill a Mockingbird";
+		short companyempy =500;
+		String country_name="United States";
+		String eye_color="Brown";
+		String birthplace= "New York City";
+        double city_dist=200.5;
+        double locationlatitude =37.7749;
+		double location_longituda=-122.4194;
 	}
 
 }
