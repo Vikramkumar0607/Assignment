70b4f98fdae73c2223c3477996b5db19f071acec
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-05-25 13:53:00 +0530

    Assignment 2 of Arrays

--- src/Assignment_Arrays.java
+++ src/Assignment_Arrays.java
@@ -2,6 +2,11 @@
 public class Assignment_Arrays {
 
 	public static void main(String[] args) {
+		
+		//Array data type can store multiple values with similar  data type together at one place
+		
+		//Syntax to initialize  array : DataType [] variable ={value1 ,value2,value3};
+		
 		String[] sem1_subjects= {"Mathematics I","Physics","Chemistry","Computer Programming","Engineering Drawing", "Basic Electrical Eng."};
 		String[] Sem1_passmarks= {"Pass(78)", "Pass(85)", "Fail(21)", "Pass(74)", "Pass(88)","Pass(79)"};
 		String[] sem2_subjects= {"Mathematics II","Mechanics","Environmental Sci.","Basic Electronics","Engineering Physics", "Engineering Graphics"};
@@ -12,6 +17,18 @@ public class Assignment_Arrays {
 		String[] Sem4_passmarks= {"Pass(91)", "Pass(73)", "Fail(19)", "Pass(80)", "Pass(76)","Pass(87)"};
 		String[] sem5_subjects= {"Probability & Stats","Machine Learning","Compiler Design","Theory of Computation","Embedded Systems", "Computer Graphics"};
 		String[] Sem5_passmarks= {"Pass(86)", "Pass(88)", "Pass(84)", "Pass(95)", "Pass(73)","Pass(90)"};
+		
+		//1D array output
+		System.out.println("Semester 2 Subject name is "+ sem2_subjects[4]);
+		System.out.println("Semester 2 Subject name is "+ sem2_subjects[5]);
+		
+		//2D array
+		String[][] semseter= {sem4_subjects,Sem4_passmarks};
+		
+		//2D arr
+		System.out.println("Semester 4 Subject 3 name is " + semseter[0][3] + " status and marks " + semseter[1][3]);
+		System.out.println("Semester 4 Subject 6 name is " + semseter[0][5] + " status and marks " + semseter[1][5]);
+		
 		
 		
 	}
