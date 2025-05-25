6b1fdcf771144489fd37b3b8a2d356249e4f87ed
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-05-25 20:06:21 +0530

    Array assignment modify

--- src/Assignment_Arrays.java
+++ src/Assignment_Arrays.java
@@ -6,34 +6,60 @@ public class Assignment_Arrays {
 		//Array data type can store multiple values with similar  data type together at one place
 		
 		//Syntax to initialize  array : DataType [] variable ={value1 ,value2,value3};
+		
 		//1D Array Semester
 		String[] Semester = {"sem1","sem2","sem3","sem4","sem5"};
-		String[] sem1_subjects= {"Mathematics I","Physics","Chemistry","Computer Programming","Engineering Drawing", "Basic Electrical Eng."};
-		String[] Sem1_passmarks= {"Pass(78)", "Pass(85)", "Fail(21)", "Pass(74)", "Pass(88)","Pass(79)"};
-		String[] sem2_subjects= {"Mathematics II","Mechanics","Environmental Sci.","Basic Electronics","Engineering Physics", "Engineering Graphics"};
-		String[] Sem2_passmarks= {"Pass(82)", "Pass(77)", "Pass(93)", "Fail(19)", "Fail(24)","Pass(90)"};
-		String[] sem3_subjects= {"Data Structures","Discrete Mathematics","Digital Electronics","Operating Systems","Signals and Systems", "Object-Oriented Prog."};
-		String[] Sem3_passmarks= {"Pass(88)", "Pass(81)", "Pass(76)", "Fail(32)", "Pass(85)","Pass(78)"};
-		String[] sem4_subjects= {"Algorithms","Computer Networks","Database Systems","Microprocessors","Communication Eng.", "Software Engineering"};
-		String[] Sem4_passmarks= {"Pass(91)", "Pass(73)", "Fail(19)", "Pass(80)", "Pass(76)","Pass(87)"};
-		String[] sem5_subjects= {"Probability & Stats","Machine Learning","Compiler Design","Theory of Computation","Embedded Systems", "Computer Graphics"};
-		
-		
-		//1D array output
-		System.out.println("Semester 2 Subject name is "+ sem2_subjects[4]);
-		System.out.println("Semester 2 Subject name is "+ sem2_subjects[5]);
-		
-		//Subjects and Status/Marks as the second dimension. 
-		String[][] sem4= {sem4_subjects,Sem4_passmarks};
-		
-		//2D arr
-		System.out.println("Semester 4 Subject 3 name is " + sem4[0][3] + " status and marks " + sem4[1][3]);
-		System.out.println("Semester 4 Subject 6 name is " + sem4[0][5] + " status and marks " + sem4[1][5]);
-		
-		
-		
-		
-		
+
+		//1D Array Semester Subjects
+		String[] Sem1_subjects= {"Mathematics I","Physics","Chemistry","Computer Programming","Engineering Drawing", "Basic Electrical Eng."};
+		String[] Sem2_subjects= {"Mathematics II","Mechanics","Environmental Sci.","Basic Electronics","Engineering Physics", "Engineering Graphics"};
+		String[] Sem3_subjects= {"Data Structures","Discrete Mathematics","Digital Electronics","Operating Systems","Signals and Systems", "Object-Oriented Prog."};
+		String[] Sem4_subjects= {"Algorithms","Computer Networks","Database Systems","Microprocessors","Communication Eng.", "Software Engineering"};
+		String[] Sem5_subjects= {"Probability & Stats","Machine Learning","Compiler Design","Theory of Computation","Embedded Systems", "Computer Graphics"};
+
+		//1D Arrary marks for sem subject wise
+		String[] Sem1_marks= {"(78)", "(85)", "(21)", "(74)", "(88)","(79)"};
+		String[] Sem2_marks= {"(82)", "(77)", "(93)", "(19)", "(24)","(90)"};
+		String[] Sem3_marks= {"(88)", "(81)", "(76)", "(32)", "(85)","(78)"};
+		String[] Sem4_marks= {"(91)", "(73)", "(19)", "(80)", "(76)","(87)"};
+		String[] Sem5_marks= {"(86)", "(88)", "(84)", "(95)", "(73)","(90)"};
+
+		//1D Arrary Subject status sem wise
+		String[] Sem1_passmarks= {"Pass", "Pass", "Fail", "Pass", "Pass","Pass"};
+		String[] Sem2_passmarks= {"Pass", "Pass", "Pass", "Fail", "Fail","Pass"};
+		String[] Sem3_passmarks= {"Pass", "Pass", "Pass", "Fail", "Pass","Pass"};
+		String[] Sem4_passmarks= {"Pass", "Pass", "Fail", "Pass", "Pass","Pass"};
+		String[] Sem5_passmarks= {"Pass", "Pass", "Pass", "Pass", "Pass","Pass"};
+
+		//2D Array of Subjects and Marks 
+		String[][] Sem1_Subject_Marks= {Sem1_subjects,Sem1_marks};
+		String[][] Sem2_Subject_Marks= {Sem2_subjects,Sem2_marks};
+		String[][] Sem3_Subject_Marks= {Sem3_subjects,Sem3_marks};
+		String[][] Sem4_Subject_Marks= {Sem4_subjects,Sem4_marks};
+		String[][] Sem5_Subject_Marks= {Sem5_subjects,Sem5_marks};
+
+		//2D Array of Subject status and Semester 
+		String[][] Sem1_Status_Sem= {Semester,Sem1_passmarks};
+		String[][] Sem2_Status_Sem= {Semester,Sem2_passmarks};
+		String[][] Sem3_Status_Sem= {Semester,Sem3_passmarks};
+		String[][] Sem4_Status_Sem= {Semester,Sem4_passmarks};
+		String[][] Sem5_Status_Sem= {Semester,Sem5_passmarks};
+
+		//3D array 
+		String[][][] Sem1_Subject_MS= {Sem1_Subject_Marks,Sem1_Status_Sem};
+		String[][][] Sem2_Subject_MS= {Sem2_Subject_Marks,Sem2_Status_Sem};
+		String[][][] Sem3_Subject_MS= {Sem3_Subject_Marks,Sem3_Status_Sem};
+		String[][][] Sem4_Subject_MS= {Sem4_Subject_Marks,Sem4_Status_Sem};
+		String[][][] Sem5_Subject_MS= {Sem5_Subject_Marks,Sem5_Status_Sem};
+
+		// Print the Status/Marks of Semester 4 Subject 3 and Subject 6.
+		System.out.println("Semester 4  subject name is "+Sem4_Subject_MS[0][0][3] +" and marks "+Sem4_Subject_MS[0][1][3]);
+		System.out.println("Semester 4  subject name is "+Sem4_Subject_MS[0][0][5] +" and marks "+Sem4_Subject_MS[0][1][5]);
+
+		//Print Semester 2 Subject 4 and Subject 5 names. 
+		System.out.println("Semester  2 subject 4 name is " +Sem2_Subject_MS[0][0][3]);
+		System.out.println("Semester  2 subject 5 name is " +Sem2_Subject_MS[0][0][4]);
+
 		
 		
 		
