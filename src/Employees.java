9d01e7489de6e1601ebeee1f41d3ca7f6e1215c1
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-05-27 09:39:34 +0530

    Employee name changes

--- src/Employees.java
+++ src/Employees.java
@@ -2,13 +2,13 @@
 public class Employees {
 
 	String Employeeone="Bharath";
-	String Employeetwo="ABC";
+	String Employeetwo="Vikram";
 	String Employeethree="XYZ";
 	int Employeeone_ID=1234;
 	int Employeetwo_ID=1235;
 	int Employeethree_ID=1236;
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
