7d7ce83da7325fc5ebe4e9e2644877e301e6f297
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-05-28 09:18:02 +0530

    Reduced the code lines

--- src/EmpGroups.java
+++ src/EmpGroups.java
@@ -2,23 +2,16 @@
 public class EmpGroups extends Employees {  //child class before extends, parent is after extends
 
 	public static void main(String[] args) {
-
-		//Employee Name array
-		String[] EmployeeName = new String [4];
-		//Employee id array
-		int [] EmployeeIDs= new int[4];
 		//Parent class object
 		Employees emp = new Employees();
 
-		EmployeeName[0] =emp.Employeeone;
-		EmployeeName[1] =emp.Employeetwo;
-		EmployeeName[2] =emp.Employeethree;
-		EmployeeName[3] =emp.Employeefour;
+		//Employee Name array
+		
+		String[] EmployeeName  ={emp.Employeeone, emp.Employeetwo, emp.Employeethree,emp.Employeefour};
+		//Employee id array
+		int [] EmployeeIDs= {emp.Employeeone_ID,emp.Employeetwo_ID,emp.Employeethree_ID,emp.Employeefour_ID};
 
-		EmployeeIDs[0] =emp.Employeeone_ID;
-		EmployeeIDs[1] =emp.Employeetwo_ID;
-		EmployeeIDs[2] =emp.Employeethree_ID;
-		EmployeeIDs[2] =emp.Employeefour_ID;
+	
 
 		//print each employee's name along with their corresponding ID.
 		System.out.println("Employee Name: " +EmployeeName[0] + ", Employee ID: " + EmployeeIDs[0]);
