8f3346da658e53dcda23512ed2ee7cb7b17a797c
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-05 08:45:46 +0530

    Array loops

--- src/ArraysLoops.java
+++ src/ArraysLoops.java
@@ -4,32 +4,33 @@ public class ArraysLoops {
 
 	public static void main(String[] args) {
 		int[] array = {1,2,3,4,5};
-		for(int i=0; i <=array.length;i++) {
-			for (int j= array.length-1; j>0 ;j-- ) {
-				 System.out.print(" ");
-				
-			}
-			for (int k=1; k<=i;k++) {
-				 System.out.print(k + " ");
-				
-			}
-			
+		for(int i=1; i <=array.length;i++) {
+			 for (int s = array.length - i; s > 0; s--) {
+	                System.out.print(" ");
+	            }
+
+			for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
             System.out.println();
+			
 
 		}
 		
-		for(int i= array.length-1;i>=1; i--) {
-			for (int j =0; j<array.length-1;j++) {
-				 System.out.print(" ");
-				
-			}
-			for(int k=1; k<=i;k++) {
-				 System.out.print(k + " ");
-				
-			}
-			System.out.println();
-		}
+		for(int i= array.length-1;i >=0; i--) {
+			 for (int s = array.length - i; s > 0; s--) {
+	                System.out.print(" ");
+	            }
 
+			for (int j = 1; j <= i; j++) {
+             System.out.print(j + " ");
+         }
+         System.out.println();
+			
+
+		}
+		
+		 
 	}
 
 }
