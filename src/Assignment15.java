b32c66e281deb632c4c1da13f9704e740acecf48
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-18 13:15:55 +0530

    assignment 15

--- src/Assignment15.java
+++ src/Assignment15.java
@@ -1,5 +1,7 @@
 import java.util.Iterator;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Assignment15 {
 
 	public static void main(String[] args) {
@@ -19,15 +21,13 @@ public class Assignment15 {
 		//Convert the first character of each word to uppercase and print original sentence
 		
 		String [] values = sentence.split(" ");
-		StringBuilder titleCaseSentence = new StringBuilder();
-		for(String value : values) {
-			if(sentence.length()>0) {
-				String capitalizedWord = value.substring(0, 1).toUpperCase() + value.substring(1);
-                titleCaseSentence.append(capitalizedWord).append(" ");
-			}
+		String finalvalue="";
+		for(int j =0; j<values.length;j++) {
+			finalvalue = finalvalue+Character.toUpperCase(values[j].charAt(0))+values[j].substring(1)+" ";
 			
 		}
-		System.out.println("Title Case Sentence: " + titleCaseSentence.toString().trim());		
+		System.out.println(finalvalue);
+		
 
 	}
 
