23184365e2742dab7acb7f7804d7667195419905
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-18 15:48:57 +0530

    trianglepattern

--- src/Assignment16.java
+++ src/Assignment16.java
@@ -5,15 +5,17 @@ public class Assignment16 {
 		String paragraph = "Java is a popular programming language. Java is used for web development, mobile applications, and more.";
 
 		String word [] =paragraph.split("");
+		System.out.println();
 		String words ="";
-		for(int i =0; i<=word.length;i++) {
+		for(int i =0; i<word.length;i++) {
+			System.out.println(word[i]);
 			words=words+word[i];
 			
-			System.out.println("Word: "+word);
-			System.out.println("Count : "+words.indexOf(i));
+			//System.out.println("Count : "+words.indexOf(i));
 			
 			
 		}
+		System.out.println("Word: "+words);
 		
 		
 	}
--- src/trianglepattern.java
+++ src/trianglepattern.java
@@ -6,12 +6,24 @@ public class trianglepattern {
 		// TODO Auto-generated method stub
 
 		int rows = 5;
-		for (int i=0; i<=rows;i++) {
-			for(int j =i; j<rows;j++) {
+		for (int i=0; i<rows;i++) {
+			for(int j =i; j<rows-1;j++) {
 				System.out.print(" ");
 			}
 			for(int k=0; k<=i;k++) {
 				System.out.print("*");
+			}
+			System.out.println("");
+		}
+
+		int row = 5;
+		for (int i=0; i<row;i++) {
+			for(int j =0; j<rows;j++) {
+				if(i+j >=row-1) {
+					System.out.print("*");
+				} else  {
+					System.out.print(" ");
+				}
 			}
 			System.out.println("");
 		}
