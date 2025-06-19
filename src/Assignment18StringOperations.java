4229d563687e0805e91d629aecc75384fdf0c778
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-19 17:03:00 +0530

    assignment 18 update

--- src/Assignment18StringOperations.java
+++ src/Assignment18StringOperations.java
@@ -1,3 +1,4 @@
+import java.util.Iterator;
 
 public class Assignment18StringOperations {
 
@@ -13,50 +14,54 @@ public class Assignment18StringOperations {
 	public static void main(String[] args) {
 		// Initialize a String
 		String S1 = "A man, a plan, a canal: Panama";
-		
+
 		//Remove all alphabets from the String ==> String.replaceAll(regExp,newChar);
 		//converting uppercase letters into lowercase letters from the String ==> String.toLowerCase();
 		String s1= S1.replaceAll("[^a-zA-Z]", "").toLowerCase();
 		//Reverse String after removing the all 
-		
+
 		//Remove all alphabets from the String ==> String.replaceAll(regExp,newChar);
 		//converting uppercase letters into lowercase letters from the String ==> String.toLowerCase();
 		String S2 = "race a car";
 		String s2 =S2.replaceAll("[^a-zA-Z]", "").toLowerCase();
-//
+		//
 		System.out.println(s1 + " is a palindrome: " + isPalindrome(s1));
 		System.out.println(s2 + " is a palindrome: " + isPalindrome(s2));
 
-		System.out.println("*****************************************************");
-		 System.out.println("Used for loop using with lowercase string length and compared");
-		  String reverseSentence = "";
-		  for(int i = s1.length()-1 ; i>=0;i--) {
-			  reverseSentence =reverseSentence+s1.charAt(i);
-		  
-		 } System.out.println("Reverse Sentence is: " + reverseSentence);
-		 
-		  if(s1.equals(reverseSentence)) {
-			  System.out.println(s1 + " is a palindrome: "  +reverseSentence); 
-			  } 
-		  else { System.out.println(s1 + " is a not palindrome: " +reverseSentence); }
-		 
-		  
-		  System.out.println("*****************************************************");
-		  System.out.println("while statement used for palindrome");
-         int i =0;
-         int j =s1.length()-1;
+		System.out.println("**********************Used for loop using with lowercase string length and compared*******************");
 
-         while(i<=j) {
-        	 if(s1.charAt(i)!=s1.charAt(j)) {
-        		 System.out.println(S1 + " is  anot  palindrome: " );
-        		 break;
-        	 }
-        	i++;
-        	j--;
-        	 
-         }
-         
-    	 System.out.println(S1 + " is a palidrome : "+ s1);
+		String reverseSentence = "";
+		for(int i = s1.length()-1 ; i>=0;i--) {
+			reverseSentence =reverseSentence+s1.charAt(i);
+
+		} System.out.println("Reverse Sentence is: " + reverseSentence);
+
+		if(s1.equals(reverseSentence)) {
+			System.out.println(s1 + " is a palindrome: "  +reverseSentence); 
+		} 
+		else { System.out.println(s1 + " is a not palindrome: " +reverseSentence); }
+
+
+		System.out.println("************************while & For & Array statement used for palindrome*****************************");
+		String [] array = {s1,s2};
+		for (int k =0; k<array.length;k++) {
+			String values=array[k];
+			int i =0;
+			int j =values.length()-1;
+			boolean falg = false;
+			while(i<=j) {
+				if(values.charAt(i)!=values.charAt(j)) {
+					falg=true;
+					break;
+				}
+				i++;
+				j--;
+
+			}
+			String s = falg?array[k] + " is  a not  palindrome: " + values : array[k] + " is  palindrome: " + values ;
+			System.out.println(s);
+
+		}
 
 	}
 
