81f0051f7c5f8dfa9bfedad0467faf73ad72c0d8
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-19 15:34:10 +0530

    assignment 18 update

--- src/Assignment18StringOperations.java
+++ src/Assignment18StringOperations.java
@@ -3,8 +3,9 @@ public class Assignment18StringOperations {
 
 
 	public static boolean isPalindrome(String str) {
+		//StringBuilder ==> StringBuilder is mutable class ==> Non-synchronized
 		String reversedStr = new StringBuilder(str).reverse().toString();
-		System.out.println(str);
+		System.out.println(reversedStr);
 		return str.equalsIgnoreCase(reversedStr);
 
 	}
@@ -16,7 +17,7 @@ public class Assignment18StringOperations {
 		//Remove all alphabets from the String ==> String.replaceAll(regExp,newChar);
 		//converting uppercase letters into lowercase letters from the String ==> String.toLowerCase();
 		String s1= S1.replaceAll("[^a-zA-Z]", "").toLowerCase();
-		
+		//Reverse String after removing the all 
 		
 		//Remove all alphabets from the String ==> String.replaceAll(regExp,newChar);
 		//converting uppercase letters into lowercase letters from the String ==> String.toLowerCase();
@@ -26,8 +27,36 @@ public class Assignment18StringOperations {
 		System.out.println(s1 + " is a palindrome: " + isPalindrome(s1));
 		System.out.println(s2 + " is a palindrome: " + isPalindrome(s2));
 
+		System.out.println("*****************************************************");
+		 System.out.println("Used for loop using with lowercase string length and compared");
+		  String reverseSentence = "";
+		  for(int i = s1.length()-1 ; i>=0;i--) {
+			  reverseSentence =reverseSentence+s1.charAt(i);
+		  
+		 } System.out.println("Reverse Sentence is: " + reverseSentence);
+		 
+		  if(s1.equals(reverseSentence)) {
+			  System.out.println(s1 + " is a palindrome: "  +reverseSentence); 
+			  } 
+		  else { System.out.println(s1 + " is a not palindrome: " +reverseSentence); }
+		 
+		  
+		  System.out.println("*****************************************************");
+		  System.out.println("while statement used for palindrome");
+         int i =0;
+         int j =s1.length()-1;
 
-
+         while(i<=j) {
+        	 if(s1.charAt(i)!=s1.charAt(j)) {
+        		 System.out.println(S1 + " is  anot  palindrome: " );
+        		 break;
+        	 }
+        	i++;
+        	j--;
+        	 
+         }
+         
+    	 System.out.println(S1 + " is a palidrome : "+ s1);
 
 	}
 
