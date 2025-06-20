24c5a94f5e27f93f1fc03d91aaacee9123cbe760
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-20 13:58:50 +0530

    assignment 19 update

---
+++ src/PowerAssignment.java
@@ -0,0 +1,35 @@
+
+public class PowerAssignment {
+	
+	public static double power(double x, int n) {
+
+		double numbertwo = 1;
+		if (n > 0) {
+			int k = 1;
+			while (k <= n) {
+				numbertwo = numbertwo * x;
+
+				k++;
+			}
+		} else {
+			int k = -1;
+			while (k >= n) {
+
+				numbertwo = numbertwo / x;
+
+				k--;
+			}
+		}
+
+		return numbertwo;
+
+	}
+
+
+	public static void main(String[] args) {
+		System.out.println("Example 1 : " + power(2.00000, 10));
+		System.out.println("Example 2 : " + power(2.10000, 3));
+		System.out.println("Example 3 : " + power(2.00000, -2));
+	}
+
+}
--- src/Powern19Assignment.java
+++ src/Powern19Assignment.java
@@ -1,9 +1,61 @@
 
 public class Powern19Assignment {
+	
+
 
 	public static void main(String[] args) {
-		double Inputx = 2.00000;
-		int powern= 10;
+
+		
+		double xn = 2.00000;
+		// Example 1:
+		System.out.println("*****Example 1*****");
+		double Input = 2.00000;
+		int powern = 10;
+		int i = 1;
+		double number = 1;
+		while (i <= powern) {
+			number = number * Input;
+
+			i++;
+		}
+		System.out.println(number);
+
+		// Example 2:
+		System.out.println("*****Example 2*****");
+		double x = 2.10000;
+		int n = 3;
+		int j = 1;
+		double numberone = 1;
+		while (j <= n) {
+			numberone = numberone * x;
+
+			j++;
+		}
+		System.out.println(numberone);
+		// Example 3:
+		System.out.println("*****Example 3*****");
+		double x1 = 2.00000;
+		int n1 = -2;
+		
+		double numbertwo = 1;
+		if (n1 > 0) {
+			int k = 1;
+			while (k <= n1) {
+				numbertwo = numbertwo * x1;
+
+				k++;
+			}
+		} else {
+			int k = -1;
+			while (k >=n1) {
+				
+				numbertwo = numbertwo / x1;
+
+				k--;
+			}
+		}
+
+		System.out.println(numbertwo);
 
 	}
 
