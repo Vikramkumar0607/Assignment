bf135e37fe49f1ed3d87e4d7a225a8de561c6a8b
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-21 11:49:30 +0530

    Funtional Interfaces

--- src/Powern19Assignment.java
+++ src/Powern19Assignment.java
@@ -56,6 +56,31 @@ public class Powern19Assignment {
 		}
 
 		System.out.println(numbertwo);
+		
+		double xx = 2.00000; // Base number (can be any real number)
+		int nn = -2;    // Exponent (can be negative, zero, or positive)
+		double result = 1; // Variable to store the final result, initialized to 1
+
+		// Case 1: If exponent is zero, any number raised to power 0 is 1
+		if (nn == 0) {
+			result = 1;
+		}
+
+		// Case 2: If exponent is negative
+		if (nn < 0) {
+			xx= 1 / xx; // Invert the base (e.g., 10 becomes 1/10)
+			nn = -nn;    // Convert the exponent to positive for easier calculation
+			System.out.println(nn);
+			
+		}
+
+		// Loop to multiply the base 'x', 'n' times
+		for(int m = 0; m < nn; m++) {
+			result = result * xx; // Multiply result with base 'x' in each iteration
+			System.out.println(result);
+		}
+		
+		System.out.println(result); // Print the final result
 
 	}
 
