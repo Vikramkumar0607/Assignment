1567cb6d5c4fd5ee62c3a840b7c262ccad590da5
Vikramkumar0607 <vkkumarm1990@gmail.com>
2025-06-23 14:56:42 +0530

    Assignment 19

--- src/Assignment19DefaultFunctionalInterfaces.java
+++ src/Assignment19DefaultFunctionalInterfaces.java
@@ -2,7 +2,20 @@
 public class Assignment19DefaultFunctionalInterfaces {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	//	InterfaceFuncations if1 =(a,b) -> a+b;
+		//System.out.println(if1.number(3,5));
+		InterfaceFuncations if1 = x -> x*x;
+		System.out.println(if1.squre(4));
+		
+		
+		//Predicate
+		//Consumer
+		//Function
+		//Supplier
+		
+		
+
 
 	}
 
---
+++ src/InterfaceFuncations.java
@@ -0,0 +1,9 @@
+
+@FunctionalInterface
+public interface InterfaceFuncations {
+
+	//public int number(int a, int b);
+	//public String message (String name);
+	public int squre(int x);
+
+}
